9da9679ac74eec075fbe899b70d9cfba2fb45a6e
kunalsingh23 <kunsingh@umich.edu>
2017-10-30 13:29:19 -0400

    pushing final HW

--- 206_HW_Twitter.py
+++ 206_HW_Twitter.py
@@ -106,7 +106,7 @@ for x in range(3):
 	query = input("Query: ")
 	print (search_twitter(query))
 	
-# I have already cached the searches of  celtics, patriots, boston, michigan, hi
+# I have already cached the searches of  "celtics", "patriots", "boston", "michigan", "hi"
 
 
 #### Recommended order of tasks: ####
